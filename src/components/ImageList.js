a4316ba9eda2a32e4f8197bfef2be539d1d17dbd
mayank <mayank>
2020-07-20 21:49:17 +0530

    change ImageList component from function to class based

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,12 +2,26 @@ import './ImageList.css';
 import React from 'react';
 import ImageCard from './ImageCard'
 
-const ImageList = (props) => {
-    const images = props.images.map(image => {
-        return <ImageCard key={image.id} image={image} />
-    });
+// const ImageList = (props) => {
+//     const images = props.images.map(image => {
+//         return <ImageCard key={image.id} image={image} />
+//     });
 
-    return <div className="image-list">{images}</div>;
+//     return <div className="image-list">{images}</div>;
+// }
+
+class ImageList extends React.Component {
+
+    setImages=()=>{
+        const images=this.props.images.map(image => {
+            return <ImageCard key={image.id} image={image} />
+        });
+        return images;
+    }
+    
+    render(){
+        return <div className="image-list">{this.setImages()}</div>;
+    }
 }
 
 export default ImageList;
